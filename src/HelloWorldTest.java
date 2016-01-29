45a84cc06f45ba2cc8678a7ee6d57565cb6ec674
Ronald Gavino <ronald.gavino@pasker.co>
2016-01-29 15:50:43 +0000

    forced fail

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -4,7 +4,7 @@ public class HelloWorldTest extends junit.framework.TestCase {
     }
     
     public void testWillAlwaysFail() {
-        fail("An error message"); 
+        fail("An error message - forcing a fail");
     }
     
 }
